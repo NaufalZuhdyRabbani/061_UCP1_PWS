898ce5e23f7d70fa936f6adcb609113081c9cc42
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2025-11-03 08:46:10 +0700

    Setup database perpustakaan_ucp with table buku in work branch

---
+++ config/database.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Koneksi gagal:', err);
+  } else {
+    console.log('Terhubung ke MySQL!');
+  }
+});
+
+export default db;
